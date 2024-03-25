617c48bee4629f69460fdfdab642fc85f34f5a34
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-03-25 16:30:54 +0100

    chercher les multipples de 3, de 5 et de 3 et 5

---
+++ jour1/job6/script.js
@@ -0,0 +1,18 @@
+
+function fizzbuzz(){
+    for (let i = 1; i <=151; i++) { // on dit : pour le compteur dans la variable on commence à 0, le compteur doit compter tant qu'il n'a pas vu tous les éléments de la liste, une fois arriver a +1 c'est la fin
+        if (i % 3 === 0 && i % 5 === 0){ // bien ouvrir les accolades pour fermer la condition et passer à la suivante
+            console.log("FizzBuzz")
+        }
+        else if(i % 3 === 0){ // il faut mettre else if parce que le if rend tout obligatoire et ne permet pas de faire apparaitre correctement la liste complète
+            console.log("Fizz")
+        }
+        else if (i % 5 === 0){
+            console.log("Buzz")
+        }
+        else{ // ne pas oublier le else pour appeler le reste des nombres de la liste
+        console.log(i) // on appele le i qui correspond au compteur qui compte entre 1 et 151
+        }   
+    }
+}
+(fizzbuzz()) // on imprime la fonction
