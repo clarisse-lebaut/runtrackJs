2b3b11cd9a10e9231176cf7ee7fce0a82d4ebb17
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-03-25 10:24:29 +0100

    affichage en console du navigateur

---
+++ jour1/job3/script.js
@@ -0,0 +1,6 @@
+/* - alert("Hello Javascript !") - */
+
+console.log("Hello Javascript !");
+console.log("J'ai trouvé la console dans le navigateur");
+console.log("Je retire le pop up en le mettant en commentaire (il est aussi possible de le supprimer")
+console.log("ça foncntionne !")
