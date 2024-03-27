c6b4d3b000f44687395ecb2ecbbf5662deefc5ca
clarisse.lebaut <clarisse.le-baut@laplateforme.io>
2024-03-27 16:38:14 +0100

    ajout brouillon sans code de job2...

---
+++ jour3/job2/script.js
@@ -0,0 +1,20 @@
+// trouver la fonction pour mélanger les images
+    // math random ?
+
+// les affilier au bouton "mélanger"
+    
+    //(document).ready(function() {
+    //$('#changeImage').click(function() {
+
+// pouvoir selectionner les images avec drag and drop
+
+    //récupérer la librairie JQuerry UI
+
+// IL FAUT 2 CONTENEURS 
+// --- une pour pouvoir mettre les images <div id=contenaire2>
+// --- une dans laquelle on récupère les images pour les placer dans le contenaire supérieur <div id=contenaire1>
+
+// conditions de réussites
+// -- faux = Vous avez perdu
+// les deux doivent prendre les bons emplacements dans le contenaire 1, si toutes les images sont à la bonne place, alors on est bon
+// -- vrai = Vous avez gagné
